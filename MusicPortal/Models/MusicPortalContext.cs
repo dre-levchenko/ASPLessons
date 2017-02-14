19cb1892755d17be1979249312b0503f1be540c0
Levchenko_A <Levchenko_A@220-10P>
2017-02-14 12:01:22 +0200

    Music Portal Initial

---
+++ MusicPortal/Models/MusicPortalContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MusicPortal.Models
+{
+    public class MusicPortalContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ MusicPortal/Models/MusicPortalDbInitializer.cs
@@ -0,0 +1,23 @@
+﻿using Guestbook.Models.Security;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace MusicPortal.Models
+{
+    public class MusicPortalDbInitializer : DropCreateDatabaseAlways<MusicPortalContext>
+    {
+        protected override void Seed(MusicPortalContext context)
+        {
+            context.Users.Add(new User
+            {
+                Name = "admin",
+                Password = MD5Hasher.ComputeHash("admin")
+            });
+
+            base.Seed(context);
+        }
+    }
+}
